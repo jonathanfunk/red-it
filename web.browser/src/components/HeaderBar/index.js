5dc02f68decf6d07c2a9ab2721980d8e863c8d48
Jonathan Funk <jonfunk88@hotmail.com>
2017-01-10 18:27:41 -0800

    styled header and side bar

--- web.browser/src/components/HeaderBar/index.js
+++ web.browser/src/components/HeaderBar/index.js
@@ -1,11 +1,25 @@
 import React, { PropTypes } from 'react';
-
 import styles from './styles.css'
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import NavigationClose from 'material-ui/svg-icons/navigation/check';
+import FlatButton from 'material-ui/FlatButton';
 
-const HeaderBar = () => {
-  return (
-    <h1 id="title">Wakawakawaka</h1>
-  );
-}
+
+/**
+ * This example uses an [IconButton](/#/components/icon-button) on the left, has a clickable `title`
+ * through the `onTouchTap` property, and a [FlatButton](/#/components/flat-button) on the right.
+ */
+const HeaderBar = () => (
+  <AppBar
+    iconElementRight={
+      <div>
+       <FlatButton label="Share a new Link" />
+       <FlatButton label="Logout" />       
+      </div>
+    }
+  />
+);
+
 
 export default HeaderBar;
--- web.browser/src/containers/Categories/index.js
+++ web.browser/src/containers/Categories/index.js
@@ -1,14 +1,22 @@
 import React, { Component, PropTypes } from 'react';
 import Week from '../../components/Week';
-import styles from './styles.css'
+import styles from './styles.css';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
 
 class Categories extends Component {
   render() {
     return (
-      <div className={styles.app}>
-        {<Week />}
-        <h2>Categories</h2>
-      </div>
+       <Drawer>
+        <div className={styles.app}>
+          <AppBar
+            title="RED it"
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+          />
+          {<Week />}
+          <h2>Categories</h2>
+        </div>
+     </Drawer>
     );
   }
 }
