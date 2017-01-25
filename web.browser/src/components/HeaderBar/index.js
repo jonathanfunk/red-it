3aba6dd3d71a06f99dabd428fb18a07f7f14108c
Jonathan Funk <jonfunk88@hotmail.com>
2017-01-25 12:26:50 -0800

    deleted documentation

--- web.browser/src/components/HeaderBar/index.js
+++ web.browser/src/components/HeaderBar/index.js
@@ -3,14 +3,10 @@ import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
 import NavigationClose from 'material-ui/svg-icons/navigation/close';
 import FlatButton from 'material-ui/FlatButton';
-//import styles from './styles.css'
 
-/**
- * This example uses an [IconButton](/#/components/icon-button) on the left, has a clickable `title`
- * through the `onTouchTap` property, and a [FlatButton](/#/components/flat-button) on the right.
- */
 const HeaderBar = () => (
-  <AppBar style={{position: 'fixed'}}
+  <AppBar
+    style={{ position: 'fixed' }}
     iconElementLeft={<IconButton><NavigationClose /></IconButton>}
     iconElementRight={<div><FlatButton label="Share a new link" style={{color: 'white'}} /><FlatButton label="Logout" style={{color: 'white'}} /></div>}
   />
