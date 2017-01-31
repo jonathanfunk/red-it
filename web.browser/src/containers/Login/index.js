3fb7294efdf5899b90ad3d2eaf219daf2823396c
Jonathan Funk <jonfunk88@hotmail.com>
2017-01-30 18:56:05 -0800

    added validator to login page

--- web.browser/src/containers/Login/index.js
+++ web.browser/src/containers/Login/index.js
@@ -1,43 +1,64 @@
 import React from 'react';
-import styles from './styles.css';
 import TextField from 'material-ui/TextField';
 import { Card } from 'material-ui/Card';
 import { Toolbar, ToolbarTitle } from 'material-ui/Toolbar';
 import FlatButton from 'material-ui/FlatButton';
+import Gandalf from '../../lib/gandalf-validator/gandalf';
+import styles from './styles.css';
 
-const Login = () => (
-  <div className={styles.login}>
-    <Card style={{ width: '500px' }}>
-      <Toolbar>
-        <ToolbarTitle text="Login" />
-      </Toolbar>
-      <form>
-        <TextField
-          style={{ width: '100%' }}
-          hintText="Email"
-          errorText="Please enter your email"
-          floatingLabelText="Email"
-          />
-        <TextField
-          style={{ width: '100%' }}
-          hintText="Password"
-          errorText="Please enter your password"
-          floatingLabelText="Password"
-          />
-        <FlatButton
-          style={{ 
-            backgroundColor: 'red',
-            color: 'white',
-          }}
-          label="Login"
-          />
-        <FlatButton
-          label="Signup"
-          />
-      </form>
-    </Card>
-  </div>
-);
+
+class Login extends Gandalf {
+  constructor() {
+    const fields = {
+      name: {
+        component: TextField,
+        validators: ['required'],
+        errorPropName: 'errorText',
+        props: {
+          hintText: 'Name',
+        },
+      },
+      email: {
+        component: TextField,
+        validators: ['required', 'email'],
+        errorPropName: 'errorText',
+        props: {
+          hintText: 'Email',
+        },
+        debounce: 300,
+      },
+    };
+
+    super(fields);
+  }
+
+  handleSubmit() {
+    const data = this.getCleanFormData();
+    if (!data) return;
+    // Submit to REDUX
+    console.log('goin\' to REDUX', data);
+  }
+
+  render() {
+    const fields = this.state.fields;
+
+    return (
+      <div className={styles.login}>
+        <Card style={{ width: '500px' }}>
+          <Toolbar>
+            <ToolbarTitle text="Login" />
+          </Toolbar>
+          <form>
+            {fields.name.element} <br />
+            {fields.email.element} <br />
+            <FlatButton label="Submit" primary onClick={() => this.handleSubmit()} />
+            <FlatButton label="Signup" />
+          </form>
+        </Card>
+      </div>
+    );
+  }
+}
 
 export default Login;
 
